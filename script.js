b2a963e974c868ab62fcf6d83a705d799019d90d
unknown <mali.lokesh5050@gmail.com>
2022-06-19 14:14:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,424 @@
+var arr = [
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "jaykapoor2.0"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Henry"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Alice"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "YashMd"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Mike"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Donna"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Louis"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Jessica"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Daniel"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Scottie"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Rachel"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Nick"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Chloe"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Paxton"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "HalfAdams"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    name: "Lana"
+  },
+
+
+];
+
+var posts = [
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+  {
+    dp: "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+    namep: "Mike Ross",
+    postp: "url()",
+    likes: "4,000 likes",
+    caption: "Mike Ross Thank You So Much #night #lights #picto #lowlight",
+    comments: "View all",
+    time: "4 HOURS AGO"
+
+  },
+]
+
+var prg = 0
+var clutter = "";
+
+arr.forEach(function (e, index) {
+  clutter += `<div class="stories">
+    <img id= "${index}" src="${e.img}" alt="">
+     <h4>${e.name}</h4>
+</div>`;
+});
+
+document.querySelector(".head").innerHTML = clutter;
+
+document.querySelector(".head").addEventListener("click", function (dets) {
+  if (dets.target.tagName === "IMG") {
+    document.querySelector("#overlay").style.display = "initial"
+    document.querySelector("#view img").setAttribute("src", arr[dets.target.id].img)
+    var timer = setInterval(function (dets) {
+      if (prg < 100) {
+        document.querySelector("#prg").style.width = prg++ + "%"
+
+      }
+      else {
+        prg = 0;
+        clearInterval(timer)
+      }
+    }, 27)
+
+    setTimeout(function (dets) {
+      document.querySelector("#overlay").style.display = "none"
+    }, 2700)
+
+  }
+})
+document.querySelector("#nav1 i").addEventListener("click", function (dets) {
+  document.querySelector("#overlay").style.display = "none"
+
+})
+
+var clutter2 = "";
+
+posts.forEach(function (dets) {
+  clutter2 += ` <div id="subhead" class="subhead">
+   <div class="postnav">
+     <div id="dp">
+       <img
+         src="https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+         alt=""
+       />
+       <div class="name">
+         <h3>Mike Ross</h3>
+         <h5>Orignal Audio</h5>
+       </div>
+     </div>
+     <i class="ri-more-fill"></i>
+   
+   </div>
+   <div class="whatispost">
+     <video src="./p1.mp4" autoplay loop muted controls></video>
+   </div>
+   <div class="like-section">
+     <div class="lef">
+       <i class="ri-heart-line"></i>
+       <i class="ri-chat-1-line"></i>
+       <i class="ri-send-plane-line"></i>
+     </div>
+     <i class="ri-bookmark-line"></i>
+   </div>
+   <h4>3,000 Likes</h4>
+   <h4 class="caption" >Mike Ross <span1>Thank You So Much #night #lighs #picto #lowlight</span1> </p></h4>
+   <h4 class="comments" >View all 38 comments</h4>
+   <h5 class="time" >4 HOURS AGO</h5>
+   <div class="do-comment">
+     <i class="ri-emotion-line"></i>
+     <input type="text" placeholder="Add a comment..." id="do-comment">
+     <h4 class="post" >Post</h4>
+   </div>
+ </div>`
+})
+document.querySelector("#all-post").innerHTML = clutter2;
+
+var openProfile = document.querySelector(".profileClick")
+
+document.querySelector("#nav-profile").addEventListener("click", function (dets) {
+  if( openProfile.style.display === "none"){
+
+    openProfile.style.display = "initial"
+  }
+  else{
+    openProfile.style.display = "none"
+  }
+})
+
+
+
+
+var msgs = [
+  {msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+  msgName:"Camille Dary",
+  whatIsMsg:"I will be there babes",
+  time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514846226882-28b324ef7f28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+  msgName:"Jessica Person",
+  whatIsMsg:"I will be there babes",
+  time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1534030347209-467a5b0ad3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Kate will",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+msgName:"Katrina Bennet",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1484329148740-e09e6c78c1e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80",
+msgName:"Daneil gillies",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1607947199747-02d4d893004f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"HArvey Specter",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1605690596358-bd352f37c4fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80",
+msgName:"Donna Paulson",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1624968731957-536b843ab2f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"RAchel Zane",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+{msgImg:"https://images.unsplash.com/photo-1514626585111-9aa86183ac98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80",
+msgName:"Camille Dary",
+whatIsMsg:"I will be there babes",
+time:"1h"
+},
+
+
+]
+
+var clutter3 = ""
+
+msgs.forEach(function(dets){
+  clutter3 += ` <div class="msg1">
+  <div class="profile">
+  <img src="${dets.msgImg}" alt="">
+  </div>
+  <div class="name">
+  <h4>${dets.msgName}</h4>
+  <h5>I will be there babes <spantime>.1h</span></h5>
+  </div>
+ </div>`
+})
+
+
+var msgSpace = document.querySelector(".msg-space")
+
+msgSpace.innerHTML = clutter3;
+
+
+const input = document.querySelector("#do-comment")
+const form = document.querySelector("#form")
+
+form.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  var chats = document.createElement("div")
+  chats.setAttribute("id", "chats")
+  chats.innerHTML = input.value;
+  input.value = "";
+  document.querySelector(".chat-space").appendChild(chats)
+})
+
+
+
+//open message feature
+
+var openChat = document.querySelector("#chat")
+var openMessages = document.querySelector(".openMessages")
+
+
+openChat.addEventListener("click", (dets) =>{
+  if(openMessages.style.display === "none"){
+
+    openMessages.style.display = "initial"
+    document.querySelector("#contents").style.display = "none"
+    }
+  else{
+    openMessages.style.display = "none"
+    document.querySelector("#contents").style.display = "initial"
+
+  }
+})
